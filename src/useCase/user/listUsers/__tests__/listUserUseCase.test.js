6d4b49ca61e2240b3b598ed9be27bc4d51b66bf2
Johnny Moreira <jmoreira@engebras.com.br>
2024-10-17 20:13:05 -0300

    fix test

---
+++ src/useCase/user/listUsers/__tests__/listUserUseCase.test.js
@@ -0,0 +1,23 @@
+import { describe, it, before, after } from 'node:test';
+import { deepStrictEqual } from 'node:assert';
+import { listUsersUseCase } from '../listUsersUseCase.js';
+import db from '../../../../infra/database/db.js';
+import { verifyAndCreateTableUsers } from '../../../../infra/database/user/table.js';
+
+describe('UNIT -> ListUserUseCase', () => {
+
+  before(() => {
+    verifyAndCreateTableUsers();
+  })
+
+  after(() => {
+    db.exec('DELETE FROM users');
+  })
+
+  it('Should return a list of users', async () => {
+    const response = await listUsersUseCase({ page: 1, pageSize: 10 });
+
+    deepStrictEqual(response, { total: 0, page: 1, pageSize: 10, users: [] });
+  });
+
+});
